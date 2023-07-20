956006275cbcb6984037505165a7b93252045476
Mamata Shee <mps.ofc.3@gmail.com>
2023-07-20 19:42:04 +0530

    Added Lottie Animation project

---
+++ Android Learning/Android in Java/LottieAnimation/app/src/main/java/com/example/lottieanimation/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.lottieanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
