4732e4886518697e4d83a16dd6d23fda0f4f0bb6
Mamata Shee <mps.ofc.3@gmail.com>
2023-10-21 22:21:58 +0530

    added fragment backstack and data passing

---
+++ Android Learning/Android in Java/25_FragmentBackstackExample/app/src/main/java/com/example/a21_fragmentexample/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.a21_fragmentexample;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity{
+    private static final String ROOT_FRAGMENT_TAG = "root_fragment";
+    Button btnFragA, btnFragB, btnFragC;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnFragA = findViewById(R.id.btnFragA);
+        btnFragB = findViewById(R.id.btnFragB);
+        btnFragC = findViewById(R.id.btnFragC);
+
+        // Default Frag Open
+        loadFragment(new AFragment(), 0);
+
+        btnFragA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadFragment(new AFragment(), 0);
+
+            }
+        });
+
+        btnFragB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadFragment(new BFragment(), 1);
+            }
+        });
+
+        btnFragC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadFragment(new CFragment(), 1);
+            }
+        });
+    }
+
+    public void loadFragment(Fragment fragment, int flag){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+//                AFragment aFragment = new AFragment();
+        if (flag == 0){
+            ft.add(R.id.container, fragment);
+            fm.popBackStack(ROOT_FRAGMENT_TAG, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+            ft.addToBackStack(ROOT_FRAGMENT_TAG);
+
+        }else{
+            ft.replace(R.id.container, fragment);
+            ft.addToBackStack(null);
+
+        }
+        ft.commit();
+    }
+}
---
+++ Android Learning/Android in Java/26_DataPassingFragmentExample/app/src/main/java/com/example/a21_fragmentexample/AFragment.java
@@ -0,0 +1,37 @@
+package com.example.a21_fragmentexample;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class AFragment extends Fragment {
+
+    public AFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        if (getArguments() != null){
+          String name =  getArguments().getString("Arg1");
+          int rollNo = getArguments().getInt("Arg2");
+          Log.d("Values from Act", "Name is: "+name + ", RollNo is: "+rollNo);
+
+        }
+
+        View view = inflater.inflate(R.layout.fragment_a, container, false);
+
+        TextView textView = view.findViewById(R.id.fragATxt);
+
+        return view;
+    }
+}
