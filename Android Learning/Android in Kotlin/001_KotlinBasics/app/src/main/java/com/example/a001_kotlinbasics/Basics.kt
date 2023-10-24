a29b1e215cfb210608cc18890ea363b573588e26
Mamata Shee <mps.ofc.3@gmail.com>
2023-10-24 18:25:30 +0530

    added when expression

--- Android Learning/Android in Kotlin/001_KotlinBasics/app/src/main/java/com/example/a001_kotlinbasics/Basics.kt
+++ Android Learning/Android in Kotlin/001_KotlinBasics/app/src/main/java/com/example/a001_kotlinbasics/Basics.kt
@@ -34,9 +34,47 @@ fun main(){
     var lastCharInStr = myString[myString.length - 1]
 
     println("First character--> "+ firstCharInStr)
-    print("Last character--> $lastCharInStr and length of my string is ${myString.length}")
+    println("Last character--> $lastCharInStr and length of my string is ${myString.length}")
 
 
+    // ************ WHEN EXPRESSION ****************
+    var season = 3
+    when(season){
+        1 -> println("Spring")
+        2 -> println("Summer")
+        3 -> {
+            println("Fall")
+            println("Autum")
+        }
+        4 -> println("Winter")
+        else -> println("Invalid Season")
+    }
+
+    var month = 3
+    when(month){
+        in 3..5 -> println("Spring")
+        in 6..8 -> println("Summer")
+        in 9..11 -> println("Fall")
+        12, 1, 2 -> println("Winter")
+        !in 1..12 -> println("Month should be between 1 to 12")
+        else -> println("Invalid Season")
+    }
+
+
+    var x: Any = 13.37
+    when(x){
+        is Int -> println("$x is an Int")
+        !is Double -> println("$x is not a Double")
+        is String -> println("$x is a String")
+        else -> println("$x is none of the above")
+    }
+
+
+    // ***************** FOOR LOOP ********************
+    // --> break and continue is same
+    for (i in 1 until 20){
+        print("$i ")
+    }
 
 }
 
